f4d9ba5d2726b51b28eb2373fff8c4bdc2380116
nandanp7 <60771317+nandanp7@users.noreply.github.com>
2020-03-29 18:58:08 +0530

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,14 @@
+
+self.addEventListener('install', evt => {
+    console.log("service worker has been installed");
+});
+
+
+self.addEventListener('activate', evt => {
+    console.log("service worker has been activated");
+});
+
+//fetch event
+self.addEventListener('fetch', evt => {
+    console.log("fetch event",evt);
+});
